57934eae53ba1d7351f9dae4a96a7beb8c121725
Rupam <www.rupamdasgupta@gmail.com>
2021-04-26 21:55:55 +0530

    frontend_done. thanks sandip

--- public/js/reg.js
+++ public/js/reg.js
@@ -1,9 +1,76 @@
+function checname(custname) {
+       
+        
+        if (custname != "") {
+            document.getElementById('nameerrormessage').style.visibility = 'hidden';
+			return true;
+            }
+        else {
+            document.getElementById('nameerrormessage').style.visibility = 'visible';
+			document.getElementById('nameerrormessage').innerText = "Enter Valid Name";
+			return false;
+			
+	   }
+}
+
+function checcontact(custmobno) {
+        mobnoexp = /\d{10}$/;
+		
+        if (mobnoexp.test(custmobno)) {
+            document.getElementById('mobnoerrormessage').style.visibility = 'hidden';
+			return true;
+        }
+        else {
+            document.getElementById('mobnoerrormessage').style.visibility = 'visible';
+            document.getElementById('mobnoerrormessage').innerText = "Enter Valid Contact No";
+			return false;
+        }
+	}
+
+function checemail(custemail) {
+        emailexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        
+        if (emailexp.test(custemail)) {
+            document.getElementById('emailerrormessage').style.visibility = 'hidden';
+			return true;
+        }
+        else {
+            document.getElementById('emailerrormessage').style.visibility = 'visible';
+            document.getElementById('emailerrormessage').innerText = "Enter Valid Email ID";
+			return false;
+        }
+    }
+
+function checpassword(custpassword) {
+		
+        
+		if (custpassword != "") {
+            document.getElementById('passerrormessage').style.visibility = 'hidden';
+			return true;
+        }
+        else {
+            document.getElementById('passerrormessage').style.visibility = 'visible';
+            document.getElementById('passerrormessage').innerText = "Enter Valid Password";
+			return false;
+        }
+    }
+
+
+
+
+
+
 function registration()
 {
 	var user  = document.getElementById("name").value ; 
 	var mobile = document.getElementById("contact").value ;
 	var email  =  document.getElementById("email").value ;
 	var pass  =   document.getElementById("pwd").value ;
+	if(checname(user) && checcontact(mobile) && checemail(email) && checpassword(pass))
+	{
+	
+	
 	var user_details =JSON.stringify({
 		name : user , 
 		mob : mobile , 
@@ -27,7 +94,7 @@ console.log(user_details);
 	
 }).catch(console.log); 
 
-	
+	}
 	
 }
 	
--- public/js/validation.js
+++ public/js/validation.js
@@ -1,102 +1,81 @@
-function checname(custname) {
-       
+function checkname() {
         
+        custname = document.getElementById("name").value;
         if (custname != "") {
             document.getElementById('nameerrormessage').style.visibility = 'hidden';
-			return true;
+			
             }
         else {
             document.getElementById('nameerrormessage').style.visibility = 'visible';
 			document.getElementById('nameerrormessage').innerText = "Enter Valid Name";
-			return false;
+			
 			
 	   }
-}
+	}
 
-function checcontact(custmobno) {
+function checkcontact() {
         mobnoexp = /\d{10}$/;
-		
+		custmobno = document.getElementById("contact").value;
         if (mobnoexp.test(custmobno)) {
             document.getElementById('mobnoerrormessage').style.visibility = 'hidden';
-			return true;
+			
         }
         else {
             document.getElementById('mobnoerrormessage').style.visibility = 'visible';
             document.getElementById('mobnoerrormessage').innerText = "Enter Valid Contact No";
-			return false;
+			
         }
 	}
 
-function checemail(custemail) {
-        emailexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+function checkemail() {
+        emailexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-        
+        custemail = document.getElementById("email").value;
         if (emailexp.test(custemail)) {
             document.getElementById('emailerrormessage').style.visibility = 'hidden';
-			return true;
+			
         }
         else {
             document.getElementById('emailerrormessage').style.visibility = 'visible';
             document.getElementById('emailerrormessage').innerText = "Enter Valid Email ID";
-			return false;
+			
         }
-}
+    }
 
-function checpassword(custpassword) {
+function checkpassword() {
 		
-        
+
+        custpassword = document.getElementById("pwd").value;
 		if (custpassword != "") {
             document.getElementById('passerrormessage').style.visibility = 'hidden';
-			return true;
+			
         }
         else {
             document.getElementById('passerrormessage').style.visibility = 'visible';
             document.getElementById('passerrormessage').innerText = "Enter Valid Password";
-			return false;
+			
         }
     }
-
-
-
-
-
-function login(){
 	
-	
-	var email  =  document.getElementById("email").value ;
-	var pass  =   document.getElementById("pwd").value ;
-	if(checemail(email) && checpassword(pass))
-	{
-	
-	var req_body = JSON.stringify({
-		"email" : email ,  
-		"pass" : pass
-	});
-	
-	console.log(req_body); 
-	
-	fetch('/user/login' , {
-		method : 'POST' , 
-		body :  req_body ,
-		headers : {
-			"accept" : "application/json" ,
-			"Content-Type"  : "application/json"
-		}
-	}).then(res=>res.json()).then(json => {
-		if(json.user_exist == true)
-		window.location.href = '/dashboard';
-		else
-		window.location.href = '/login';
-		
-	}).catch(console.log) ; 
-	
-	}
-	
-	
-	
-	
+function clearname()
+{
+	document.getElementById("name").style.background = 'White';
+	document.getElementById('nameerrormessage').style.visibility = 'hidden';
 	
 }
 
+function clearemail()
+{
+	document.getElementById("email").style.background = 'White';
+	document.getElementById('emailerrormessage').style.visibility = 'hidden';
+	
+}
+
+function clearcontact()
+{
+	document.getElementById("contact").style.background = 'White';
+	document.getElementById('mobnoerrormessage').style.visibility = 'hidden';
+	
+}
 
 
